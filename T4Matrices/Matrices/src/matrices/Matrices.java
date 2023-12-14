6f481fcbec24bfebf599b25e94c82ccdcc19929c
Cesar davila <cesardavila121204@gmail.com>
2023-12-14 21:01:25 +0100

    Matrices contiene juego

--- T4Matrices/Matrices/src/matrices/Matrices.java
+++ T4Matrices/Matrices/src/matrices/Matrices.java
@@ -1,35 +1,85 @@
 package matrices;
 
+import java.util.Scanner;
+
 public class Matrices {
 
     public static void main(String[] args) {
-        int min = 100, max = 1000;
-        int[][] matri = new int[4][5];
-        int[][] sumas = new int[2][matri[1].length];
-        for (int i = 0; i < matri.length; i++) {
-            for (int j = 0; j < matri[i].length; j++) {
-                matri[i][j] = (int) (Math.random() * (max - min) + min);
-                sumas[0][j] += matri[i][j];
-                sumas[1][i] += matri[i][j];
-                sumas[1][sumas[0].length - 1] += matri[i][j];
+        Scanner entrada = new Scanner (System.in);
+        int tecla;
+        int count = 0;
+        String pelota = "o";
+        int espacios = 10;
+        int espaciosP = 12;
+        do {
+            switch (count) {
+                case 0:
+                    System.out.println(" ".repeat(espaciosP) + pelota);
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    
+                    break;
+                case 1:
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP) + pelota);
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    break;
+                case 2:
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP) + pelota);
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    break;
+                case 3:
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP) + pelota);
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    break;
+                case 4:
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP) + pelota);
+                    System.out.println(" ".repeat(espaciosP));
+                    break;
+                case 5:
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP));
+                    System.out.println(" ".repeat(espaciosP) + pelota);
+                    break;
+                default:
+                    
             }
-        }
-        for (int i = 0; i < matri.length; i++) {
-            for (int j = 0; j < matri[i].length; j++) {
-                System.out.printf("%10s", matri[i][j]);
-
+            System.out.println(" ".repeat(espacios) + "-".repeat(5));
+            tecla = entrada.nextInt();
+            switch (tecla) {
+                case 1:
+                    espacios--;
+                    break;
+                case 3:
+                    espacios++;
+                    break;
+                default:  
             }
-            System.out.printf("%4s", "|");
-            System.out.printf("%10s", sumas[1][i] + "\n");
-            if (i == matri.length) {
-                System.out.print("-".repeat(matri[1].length * 15) + "\n");
-            }
-        }
-        System.out.print("-".repeat(matri[1].length * 15) + "\n");
-        for (int j = 0; j < sumas[0].length; j++) {
-            System.out.printf("%10s", sumas[0][j]);
-        }
-        System.out.printf("%4s", "|");
-        System.out.printf("%10s", sumas[1][sumas[0].length-1] + "\n");
+            if (count == 10) {
+                count = 0;
+            } 
+            count++;
+        } while (true);
     }
 }
